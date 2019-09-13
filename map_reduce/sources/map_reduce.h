fde7c7e633181fb0187c6108002ea52695c26eb1
Pavel <pvastig@gmail.com>
2019-09-13 21:38:28 +0700

    Use std::thread for map reduce

--- map_reduce/sources/map_reduce.h
+++ map_reduce/sources/map_reduce.h
@@ -1,8 +1,11 @@
+#include <algorithm>
+#include <cassert>
 #include <future>
+#include <thread>
 #include <iostream>
 #include <list>
 #include <vector>
-#include <cassert>
+#include <numeric>
 
 using namespace std;
 
@@ -17,11 +20,18 @@ auto map_reduce(It p, It q, UnaryOp f1, BinaryOp f2, size_t threads)->decltype(f
         return res;
     };
 
-    std::vector<std::future<decltype(f1(*p))>> result;
-    result.reserve(threads);
+    using resType = decltype(f1(*p));
+    std::vector<std::future<resType>> result(threads);
     auto const len  = std::distance(p, q);
+    if (!len)
+        return resType{};
+
+    if (threads > len)
+        threads = 2;
+
     auto const r    = static_cast<size_t>(len) % threads;
     auto const step = static_cast<size_t>(len) / threads;
+    //TODO: rework using iterators
     result.push_back(std::async(std::launch::async, calcPartSum, p, step+r));
     advance(p, step + r);
     for (size_t i = 1; i < threads; ++i)
@@ -38,7 +48,7 @@ auto map_reduce(It p, It q, UnaryOp f1, BinaryOp f2, size_t threads)->decltype(f
 }
 
 template<class It, class UnaryOp, class BinaryOp>
-auto map_reduce2(It p, It q, UnaryOp f1, BinaryOp f2)
+auto map_reduce_one_thread(It p, It q, UnaryOp f1, BinaryOp f2)
 {
     auto res = f1(*p);
     while(++p != q)
@@ -48,18 +58,55 @@ auto map_reduce2(It p, It q, UnaryOp f1, BinaryOp f2)
     return res;
 }
 
+template<class It, class UnaryOp, class BinaryOp>
+auto map_reduce2(It p, It q, UnaryOp f1, BinaryOp f2, size_t numThreads)->decltype(f1(*p))
+{
+    auto calcPartSum = [f1, f2](It it, size_t dist)
+    {
+        auto res = f1(*it);
+        for (decltype(dist) i = 1; i < dist; ++i)
+            res = f2(res, f1(*++it));
+        return res;
+    };
+
+    std::vector<std::thread> threads(numThreads);
+    std::vector<decltype(f1(*p))> results(numThreads);
+    auto const len  = std::distance(p, q);
+    auto const r    = static_cast<size_t>(len) % numThreads;
+    auto const step = static_cast<size_t>(len) / numThreads;
+    threads.push_back(std::thread([&results, p, step = step + r, calcPartSum](){results[0] = calcPartSum(p, step);}));
+    advance(p, step + r);
+    for (size_t i = 1; i < numThreads; ++i)
+    {
+        threads.push_back(std::thread([&results,i, p, step, calcPartSum]() { results[i] =calcPartSum(p, step);}));
+        advance(p, step);
+    }
+
+    for (auto & thread : threads)
+    {
+        if(thread.joinable())
+            thread.join();
+    }
+
+    auto res = *results.begin();
+    for (auto it = std::next(results.begin()); it != results.end(); ++it)
+        res = f2(res, *it);
+
+    return res;
+}
+
 void runMapReduce()
 {
     {
         std::list<int> l = {1,2,3,4,5,6,7,8,9,10};
         for (size_t i = 1; i <= 10; ++i)
         {
-            auto sum = map_reduce(l.begin(), l.end(),
+            auto sum = map_reduce2(l.begin(), l.end(),
                 [](int i){return i;},
                 std::plus<int>(), i);
             assert(sum == 55);
 
-            auto has_even = map_reduce(l.begin(), l.end(),
+            auto has_even = map_reduce2(l.begin(), l.end(),
                 [](int i){return i % 2 == 0;},
                 std::logical_or<bool>(), i);
             assert(has_even == 1);
@@ -70,12 +117,12 @@ void runMapReduce()
         std::list<int> l = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         for (size_t i = 1; i <= 10; ++i)
         {
-            auto sum = map_reduce(l.begin(), l.end(),
+            auto sum = map_reduce2(l.begin(), l.end(),
                 [](int i){return i;},
                 std::plus<int>(), i);
             assert(sum == 10);
 
-            auto has_even = map_reduce(l.begin(), l.end(),
+            auto has_even = map_reduce2(l.begin(), l.end(),
                 [](int i){return i % 2 == 0;},
                 std::logical_or<bool>(), i);
             assert(has_even == 0);
